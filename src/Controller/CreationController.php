6c4c9c2d42a3e17b603f847456e12047ae6ecc25
Smaga Michaele <smaga.michael@bbox.fr>
2021-11-21 20:55:25 +0100

    rangement css galery

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CreationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,10 +22,13 @@ class CreationController extends AbstractController
      */
 
     #[Route('/creation', name: 'creation')]
-    public function index(): Response
+    public function index(CreationRepository $creationRepository): Response
     {
+
+        $creations = $creationRepository->findAll();
+
         return $this->render('creation/index.html.twig', [
-            'controller_name' => 'CreationController',
+            'creations' =>  $creations,
         ]);
     }
 }
