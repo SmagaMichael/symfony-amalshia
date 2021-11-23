d4c3ce75035da1db2cc2f67807bb791ad1c75be6
Smaga Michaele <smaga.michael@bbox.fr>
2021-11-23 07:35:00 +0100

    suppression d'une création

---
+++ src/Controller/DeleteCreationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Creation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DeleteCreationController extends AbstractController
+{
+    #[Route('/delete/creation/{id}', name: 'delete_creation')]
+    public function index(Creation $creation): Response
+    {
+               // Pour supprimer avec Doctrine
+               $entityManager = $this->getDoctrine()->getManager();
+               $entityManager->remove($creation);
+               $entityManager->flush(); // DELETE FROM
+       
+               $this->addFlash('danger', 'La création a bien été supprimée');
+       
+               return $this->redirectToRoute('creation');
+    }
+}
