d1027f0fe9e2018addb42fe21189ce6637fd779f
Smaga Michaele <smaga.michael@bbox.fr>
2021-11-27 21:49:36 +0100

    suppression image quand la création est supprimé

--- src/Controller/DeleteCreationController.php
+++ src/Controller/DeleteCreationController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Creation;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DeleteCreationController extends AbstractController
 {
@@ -15,6 +16,18 @@ class DeleteCreationController extends AbstractController
                // Pour supprimer avec Doctrine
                $entityManager = $this->getDoctrine()->getManager();
                $entityManager->remove($creation);
+
+
+               if ($creation->getPicture()) {
+                // FileSystem permet de manipuler les fichiers
+                $fs = new Filesystem();
+                // On supprime l'ancienne image
+                $fs->remove($this->getParameter('upload_directory').'/'.$creation->getPicture());
+            }
+
+         
+        
+
                $entityManager->flush(); // DELETE FROM
        
                $this->addFlash('danger', 'La création a bien été supprimée');
