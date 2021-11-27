15aae0b0857e3d8672a3b990a8bd24d784f5bdc7
Smaga Michaele <smaga.michael@bbox.fr>
2021-11-27 03:34:43 +0100

    autre script pour le texte animé

--- public/js/anime-text.js
+++ public/js/anime-text.js
@@ -1,43 +1,34 @@
 $( ".button-avatar" ).click(function() {
 // $( ".button-avatar" ).click(function() {
     $(".talk-bubble").removeAttr('hidden');
-    // $(".button-avatar").addAttr('disabled');
-    //on récupère le contenu du texte dans la ballise P ID="MyTextDescription"
-var TexteEntier = document.getElementById('MyTextDescription').innerText;
-//On récupére le nombre de caractère du contenu
-var nombre_de_lettre = TexteEntier.length; 
-
-//On ajoute chaque lettre dans un tableau 
-var tableau = TexteEntier.split("");
-
-texte = new Array;
-var txt = '';
-var nb_msg = nombre_de_lettre - 1;
-// console.log(txt);
-
-
-//pour i = 0 on incrémente jusqu'à ce qu'on ai atteint le nombre de lettre du contenu
-for (i=0;  i < nombre_de_lettre;  i++) {
-texte[i] = txt+tableau[i];
-var txt = texte[i];
-
-
-
-// console.log(txt);
-
-}
-
-actual_texte = 0;
-
-function changeMessage(){
-document.getElementById("MyTextDescription").innerHTML = texte[actual_texte];
-actual_texte++;
-if(actual_texte >= texte.length)
-actual_texte = nb_msg;
-}
-
-
-setInterval(changeMessage,40) /* la vitesse de defilement (plus on a une valeur faible plus 
-texte s'affiche rapidement) */
+    "use strict";
+    $(document).ready(function() {
+      animate_text();
+    });
+    // -------------------
+    function animate_text() 
+    {
+      let delay = 50,
+          delay_start = 0,
+          contents,
+          letters;
+    
+      $(".animate-text").each(function(index, obj) {
+        contents = $(obj).text().trim();
+        $(obj).html(''); // on vide le contenu
+        letters = contents.split("");
+        $(letters).each(function(index_1, letter) {
+          setTimeout(function() {
+            // ---------
+            // effet machine à écrire simple
+            $(obj).html( $(obj).html() + letter ); // on ajoute chaque lettre l une après l autre
+            // ---------
+            // ---------
+          }, delay_start + delay * index_1);
+        });
+        // le suivant démarre à la fin du précédent
+        delay_start += delay * letters.length;
+      });
+    }
 
   });
